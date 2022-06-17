07b6e0ae01d2e9b40d9c1177f6020682ccee353e
Ethanol-14 <69091472+Ethanol-14@users.noreply.github.com>
2022-06-16 23:23:19 -0700

    Add files via upload

--- main.java
+++ main.java
@@ -1,40 +1,140 @@
-public class main {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class main extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private static int[] drawnNums = new int[0];
+	private static int drawnLargest = 0;
+	
+	private static int greenIndex = 0;
+	private static int greenInt = 0;
+	private static int redIndex = 0;
+	private static int redInt = 0;
+	
+	private static JPanel panel = new main();
+	private static JFrame frame = new JFrame("The sight of sorting"); //title for the window
+	
+	private static int borderWidth = 200;
+
 	public static void main(String[] args) {
 		
-		int tests = 1; //the amount of tests you want to run
-		int arrayLength = 1000; //the length of the array you want to shuffle and sort
-		int delay = 1; //delay in milliseconds, so you can actually watch things happen in front of your slow human eyes
+		int arrayLength = 64; //the length of the array you want to shuffle and sort
+		int delay = 10; //delay in milliseconds, so you can actually watch things happen in front of your slow human eyes
+				
+		final int width = 1280;
+		final int height = 720;
 		
-		int largest = 0;
+		panel.setBackground(Color.black); //window's panel's background
+		frame.setSize(width, height); //frame resolution
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //allows the program to terminate when the window is closed
+		frame.getContentPane().add(panel, BorderLayout.CENTER); //puts the panel inside the window...?
+		frame.setVisible(true); //allows window to appear (idk why tf this isn't the default setting)
+		
+		run(arrayLength, delay, 0);
+	}
+	
+	private static void run(int arrayLength, int delay, int sortType) {
 		int[] nums = new int[arrayLength];
+		int largest = 0;
 		
 		for (largest = 0; largest < nums.length; largest++) {
 			nums[largest] = largest;
 		}
 		largest--;
 		
-		//initialize window
-		visualize.init(arrayLength, largest);
+		//initialize array
+		setLengthAndLargest(arrayLength, largest);
+		//shuffle
+		nums = visualSort.shuffle(nums);
+		updateArray(nums, 0, nums.length-1);
 		
-		for (int loop = 0; loop < tests; loop++) {
-			//shuffle
-			nums = sort.shuffle(nums);
-			visualize.updateArray(nums, 0, nums.length-1);
-			
-			//sort and print
-			nums = visualSort.bucket(nums, delay, 0, nums.length-1);
-			
-			//check if sorted
-			if (sort.check(nums)) {
-				System.out.println("Correctly sorted\n");
-			}
-			else {
-				System.out.println("Incorrectly sorted\n");
-			}
-			System.out.println("Comparisons: "+visualSort.getComparisons());
-			System.out.println("Array accesses: "+visualSort.getArrayAccesses());
-			sort.reset();
+		//sort and print
+		if (sortType == 0) {
+			nums = visualSort.selection(nums, delay, 0, nums.length-1);
 		}
+		if (sortType == 1) {
+			nums = visualSort.bubble(nums, delay, 0, nums.length-1);
+		}
+		if (sortType == 2) {
+			nums = visualSort.insertion(nums, delay, 0, nums.length-1);
+		}
+		if (sortType == 3) {
+			nums = visualSort.comb(nums, delay, 0, nums.length-1);
+		}
+		if (sortType == 4) {
+			nums = visualSort.merge(nums, delay, 0, nums.length-1);
+		}
+		if (sortType == 5) {
+			nums = visualSort.quick(nums, delay, 0, nums.length-1);
+		}
+		if (sortType == 6) {
+			nums = visualSort.counting(nums, delay, 0, nums.length-1);
+		}
+		if (sortType == 7) {
+			nums = visualSort.bucket(nums, delay, 0, nums.length-1);
+		}
+		
+		//check if sorted
+		if (visualSort.check(nums)) {
+			System.out.println("Correctly sorted\n");
+		}
+		else {
+			System.out.println("Incorrectly sorted\n");
+		}
+		System.out.println("Comparisons: "+visualSort.getComparisons());
+		System.out.println("Array accesses: "+visualSort.getArrayAccesses());
+		visualSort.reset();
+	}
+	
+	private static void setLengthAndLargest(int _length, int _largest) {
+		drawnNums = new int[_length];
+		drawnLargest = _largest;
+	}
+	
+	public static void updateArray(int[] _nums, int firstIndex, int secondIndex) {
+		for (int i = firstIndex; i <= secondIndex; i++) {
+			drawnNums[i] = _nums[i-firstIndex];
+		}
+		frame.repaint();
+	}
+	
+	public static void setGreens(int num, int index) { //represents comparisons
+		greenIndex = index;
+		greenInt = num;
+		frame.repaint();
+	}
+	
+	public static void setReds(int num, int index) { //represents array accesses
+		redIndex = index;
+		redInt = num;
+		frame.repaint();
+	}
+	
+	public static void updateIndex(int num, int index) {
+		drawnNums[index] = num;
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.setColor(Color.white);
+		
+		int actualWidth = getWidth()-borderWidth;
+		
+		for (int i = 0; i < drawnNums.length; i++) {	
+			g.fillRect((i*actualWidth)/drawnNums.length, getHeight()-((drawnNums[i]*getHeight())/drawnLargest), (actualWidth/drawnNums.length)+1, (drawnNums[i]*getHeight())/drawnLargest);
+		}
+		
+		g.setColor(Color.green);
+		g.fillRect((greenIndex*actualWidth)/drawnNums.length, getHeight()-((greenInt*getHeight())/drawnLargest), (actualWidth/drawnNums.length)+1, (greenInt*getHeight())/drawnLargest);
+		g.setColor(Color.red);
+		g.fillRect((redIndex*actualWidth)/drawnNums.length, getHeight()-((redInt*getHeight())/drawnLargest), (actualWidth/drawnNums.length)+1, (redInt*getHeight())/drawnLargest);
 	}
 	
 	private static String arrayToString(int[] nums) {
--- visualSort.java
+++ visualSort.java
@@ -36,25 +36,25 @@ public class visualSort {
 				if (nums[smallestNumsIndex] < nums[finalSmallestNumIndex]) { //finds the index of the smallest integer
 					finalSmallestNumIndex = smallestNumsIndex;
 				}
-				visualize.setGreens(nums[smallestNumsIndex], smallestNumsIndex);
+				main.setGreens(nums[smallestNumsIndex], smallestNumsIndex);
 				comparisons++;
 				delay(delay);
 			}
 			//swaps the index of the smallest integer with the largest index you KNOW is sorted (starting at 0, then 1, then 2, etc.)
 			smallestNum = nums[finalSmallestNumIndex];
 			nums[finalSmallestNumIndex] = nums[largestSortedIndex];
-			visualize.setReds(nums[largestSortedIndex], finalSmallestNumIndex);
-			visualize.updateIndex(nums[largestSortedIndex], finalSmallestNumIndex);
+			main.setReds(nums[largestSortedIndex], finalSmallestNumIndex);
+			main.updateIndex(nums[largestSortedIndex], finalSmallestNumIndex);
 			nums[largestSortedIndex] = smallestNum;
-			visualize.setReds(smallestNum, largestSortedIndex);
-			visualize.updateIndex(smallestNum, largestSortedIndex);
-			visualize.setGreens(smallestNum, largestSortedIndex);
+			main.setReds(smallestNum, largestSortedIndex);
+			main.updateIndex(smallestNum, largestSortedIndex);
+			main.setGreens(smallestNum, largestSortedIndex);
 			arrayAccesses += 2;
 			delay(delay*2);
 		}
 
-		visualize.setGreens(0, 0);
-		visualize.setReds(0, 0);
+		main.setGreens(0, 0);
+		main.setReds(0, 0);
 		return nums;
 	}
 	
@@ -69,12 +69,12 @@ public class visualSort {
 				if (nums[i] > nums[i+1]) { //basically, sorts every single pair of adjacent integers, eventually sorting everything
 					largerInt = nums[i];
 					nums[i] = nums[i+1];
-					visualize.setReds(nums[i+1], i);
-					visualize.updateIndex(nums[i+1], i);
+					main.setReds(nums[i+1], i);
+					main.updateIndex(nums[i+1], i);
 					nums[i+1] = largerInt;
-					visualize.setReds(largerInt, i+1);
-					visualize.updateIndex(largerInt, i+1);
-					visualize.setGreens(largerInt, i+1);
+					main.setReds(largerInt, i+1);
+					main.updateIndex(largerInt, i+1);
+					main.setGreens(largerInt, i+1);
 					swaps++;
 					arrayAccesses += 2;
 					delay(delay*2);
@@ -83,14 +83,14 @@ public class visualSort {
 				delay(delay);
 			}
 			if (swaps == 0) { //counts the number of swaps performed, essentially checking if the list is sorted
-				visualize.setGreens(0, 0);
-				visualize.setReds(0, 0);
+				main.setGreens(0, 0);
+				main.setReds(0, 0);
 				return nums;
 			}
 		}
 
-		visualize.setGreens(0, 0);
-		visualize.setReds(0, 0);
+		main.setGreens(0, 0);
+		main.setReds(0, 0);
 		return nums;
 	}
 	
@@ -104,12 +104,12 @@ public class visualSort {
 			while (i != 0 && nums[i] < nums[i-1]) {
 				largerInt = nums[i];
 				nums[i] = nums[i-1];
-				visualize.setReds(nums[i], i);
-				visualize.updateIndex(nums[i], i);
+				main.setReds(nums[i], i);
+				main.updateIndex(nums[i], i);
 				nums[i-1] = largerInt;
-				visualize.setReds(nums[i-1], i-1);
-				visualize.updateIndex(nums[i-1], i-1);
-				visualize.setGreens(nums[i-1], i-1);
+				main.setReds(nums[i-1], i-1);
+				main.updateIndex(nums[i-1], i-1);
+				main.setGreens(nums[i-1], i-1);
 				i--;
 				comparisons++;
 				arrayAccesses += 2;
@@ -117,8 +117,8 @@ public class visualSort {
 			}
 		}
 
-		visualize.setGreens(0, 0);
-		visualize.setReds(0, 0);
+		main.setGreens(0, 0);
+		main.setReds(0, 0);
 		return nums;
 	}
 	
@@ -132,29 +132,29 @@ public class visualSort {
 			swaps = 0;
 			
 			for (int i = 0; i < nums.length-(gap+1); i++) {
-				visualize.setGreens(nums[i], i);
+				main.setGreens(nums[i], i);
 				if (nums[i] > nums[i+gap+1]) {
 					largerInt = nums[i];
 					nums[i] = nums[i+gap+1];
-					visualize.setReds(nums[i], i);
-					visualize.updateIndex(nums[i], i);
+					main.setReds(nums[i], i);
+					main.updateIndex(nums[i], i);
 					delay(delay);
 					nums[i+gap+1] = largerInt;
-					visualize.setReds(nums[i+gap+1], i+gap+1);
-					visualize.updateIndex(nums[i+gap+1], i+gap+1);
+					main.setReds(nums[i+gap+1], i+gap+1);
+					main.updateIndex(nums[i+gap+1], i+gap+1);
 					delay(delay);
 					arrayAccesses += 2;
 					swaps++;
 				}
 				comparisons++;
 				delay(delay);
-				visualize.setGreens(0, 0);
-				visualize.setReds(0, 0);
+				main.setGreens(0, 0);
+				main.setReds(0, 0);
 			}
 		}
 
-		visualize.setGreens(0, 0);
-		visualize.setReds(0, 0);
+		main.setGreens(0, 0);
+		main.setReds(0, 0);
 		return nums;
 	}
 	
@@ -167,13 +167,13 @@ public class visualSort {
 			
 			for (int i = 0; i < nums.length/2; i++) {
 				firstHalf[i] = nums[i];
-				visualize.setGreens(nums[i], i+_smallest);
+				main.setGreens(nums[i], i+_smallest);
 				arrayAccesses++;
 				delay(delay);
 			}
 			for (int i = nums.length/2; i < nums.length; i++) {
 				secondHalf[i-nums.length/2] = nums[i];
-				visualize.setGreens(nums[i], i+_smallest);
+				main.setGreens(nums[i], i+_smallest);
 				arrayAccesses++;
 				delay(delay);
 			}
@@ -184,17 +184,17 @@ public class visualSort {
 			//now, join the halves
 			for (int i = 0; i < nums.length/2; i++) {
 				nums[i] = firstHalf[i];
-				visualize.setGreens(nums[i], i+_smallest);
+				main.setGreens(nums[i], i+_smallest);
 				arrayAccesses++;
 				delay(delay);
 			}
 			for (int i = nums.length/2; i < nums.length; i++) {
 				nums[i] = secondHalf[i-nums.length/2];
-				visualize.setGreens(nums[i], i+_smallest);
+				main.setGreens(nums[i], i+_smallest);
 				arrayAccesses++;
 				delay(delay);
 			}
-			visualize.setGreens(0, 0);
+			main.setGreens(0, 0);
 			comparisons++;
 			delay(delay);
 		}
@@ -234,14 +234,14 @@ public class visualSort {
 				comparisons++;
 				delay(delay);
 			}
-			visualize.setReds(memoryNums[i], _smallest+i);
-			visualize.updateIndex(memoryNums[i], _smallest+i);
+			main.setReds(memoryNums[i], _smallest+i);
+			main.updateIndex(memoryNums[i], _smallest+i);
 			comparisons += 2;
 			delay(delay*2);
 		}
 
-		visualize.setGreens(0, 0);
-		visualize.setReds(0, 0);
+		main.setGreens(0, 0);
+		main.setReds(0, 0);
 		return memoryNums;
 	}
 	
@@ -257,8 +257,8 @@ public class visualSort {
 			//we'll start by putting all the smaller integers from left to right
 			if (nums[i] < nums[0]) {
 				
-				visualize.setReds(nums[i], _smallest+smallersIndex);
-				visualize.updateIndex(nums[i], _smallest+smallersIndex);
+				main.setReds(nums[i], _smallest+smallersIndex);
+				main.updateIndex(nums[i], _smallest+smallersIndex);
 				
 				memoryNums[smallersIndex] = nums[i];
 				smallersIndex++;
@@ -268,8 +268,8 @@ public class visualSort {
 			//then put all the larger integers from right to left
 			else {
 				
-				visualize.setReds(nums[i], _largest-largersProgress);
-				visualize.updateIndex(nums[i], _largest-largersProgress);
+				main.setReds(nums[i], _largest-largersProgress);
+				main.updateIndex(nums[i], _largest-largersProgress);
 				
 				memoryNums[largersIndex] = nums[i];
 				largersIndex--;
@@ -280,12 +280,12 @@ public class visualSort {
 			comparisons++;
 			delay(delay);
 		}
-		visualize.setReds(0, 0);
+		main.setReds(0, 0);
 		//then, put the first integer in the remaining gap
 		memoryNums[smallersIndex] = nums[0];
-		visualize.setReds(nums[0], _smallest+smallersIndex);
-		visualize.updateIndex(nums[0], _smallest+smallersIndex);
-		visualize.setReds(0, 0);
+		main.setReds(nums[0], _smallest+smallersIndex);
+		main.updateIndex(nums[0], _smallest+smallersIndex);
+		main.setReds(0, 0);
 		arrayAccesses++;
 		delay(delay);
 		
@@ -294,20 +294,20 @@ public class visualSort {
 			int[] smallers = new int[smallersIndex];
 			for (int i = 0; i < smallers.length; i++) {
 				smallers[i] = memoryNums[i];
-				visualize.setGreens(smallers[i], i+_smallest);
+				main.setGreens(smallers[i], i+_smallest);
 				arrayAccesses++;
 				delay(delay);
 			}
-			visualize.setGreens(0, 0);
+			main.setGreens(0, 0);
 			smallers = quick(smallers, delay, _smallest, _smallest+smallersIndex-1);
 			//then, get our sorted section back, and put them back into memoryNums
 			for (int i = 0; i < smallers.length; i++) {
 				memoryNums[i] = smallers[i];
-				visualize.setGreens(smallers[i], i+_smallest);
+				main.setGreens(smallers[i], i+_smallest);
 				arrayAccesses++;
 				delay(delay);
 			}
-			visualize.setGreens(0, 0);
+			main.setGreens(0, 0);
 			comparisons++;
 			delay(delay);
 		}
@@ -316,27 +316,27 @@ public class visualSort {
 			int[] largers = new int[memoryNums.length-(smallersIndex+1)];
 			for (int i = 0; i < largers.length; i++) {
 				largers[i] = memoryNums[i+smallersIndex+1];
-				visualize.setGreens(largers[i], i+_smallest+smallersIndex+1);
+				main.setGreens(largers[i], i+_smallest+smallersIndex+1);
 				arrayAccesses++;
 				delay(delay);
 			}
-			visualize.setGreens(0, 0);
+			main.setGreens(0, 0);
 			largers = quick(largers, delay, _smallest+smallersIndex+1, _largest);
 			//then, get our sorted section back, and put them back into memoryNums
 			for (int i = 0; i < largers.length; i++) {
 				memoryNums[i+smallersIndex+1] = largers[i];
-				visualize.setGreens(largers[i], i+_smallest+smallersIndex+1);
+				main.setGreens(largers[i], i+_smallest+smallersIndex+1);
 				arrayAccesses++;
 				delay(delay);
 			}
-			visualize.setGreens(0, 0);
+			main.setGreens(0, 0);
 			comparisons++;
 			delay(delay);
 		}
 		
 		//finally, return out sorted section. Eventually, our "section" will be stitched back up as the whole array, and we'll do one final return!
-		visualize.setGreens(0, 0);
-		visualize.setReds(0, 0);
+		main.setGreens(0, 0);
+		main.setReds(0, 0);
 		return memoryNums;
 	}
 	
@@ -352,7 +352,7 @@ public class visualSort {
 			if (nums[i] < smallest) {
 				smallest = nums[i];
 			}
-			visualize.setGreens(nums[i], i);
+			main.setGreens(nums[i], i);
 			comparisons += 2;
 			delay(delay*2);
 		}
@@ -364,11 +364,11 @@ public class visualSort {
 		for (int i = 0; i < nums.length; i++) {
 			//the reason we subtract nums[i] by smallest is to set the smallest integer at index 0, because what if nums[i] = -1? then what?
 			occurences[nums[i]-smallest]++;
-			visualize.setGreens(nums[i], i);
+			main.setGreens(nums[i], i);
 			arrayAccesses++;
 			delay(delay);
 		}
-		visualize.setGreens(0, 0);
+		main.setGreens(0, 0);
 		
 		//the thing is, occurences is sorted! it just doesn't represent the values in the way we want
 		//so we can refill our orignal array with the occurences of each index
@@ -379,15 +379,15 @@ public class visualSort {
 		for (int i = 0; i < occurences.length; i++) {
 			for (int x = 0; x < occurences[i]; x++) {
 				nums[cumSumOcc+x] = i+smallest;
-				visualize.setReds(i+smallest, cumSumOcc+x);
-				visualize.updateIndex(i+smallest, cumSumOcc+x);
+				main.setReds(i+smallest, cumSumOcc+x);
+				main.updateIndex(i+smallest, cumSumOcc+x);
 				arrayAccesses++;
 				delay(delay);
 			}
 			cumSumOcc += occurences[i];
 		}
 
-		visualize.setReds(0, 0);
+		main.setReds(0, 0);
 		return nums;
 	}
 	
@@ -403,7 +403,7 @@ public class visualSort {
 			if (nums[i] > largest) {
 				largest = nums[i];
 			}
-			visualize.setGreens(nums[i], i);
+			main.setGreens(nums[i], i);
 			comparisons++;
 			delay(delay);
 		}
@@ -419,11 +419,11 @@ public class visualSort {
 					digit /= 10;
 				}				
 				buckets[digit%10]++; //get the value of that digit we want to look at and at it to the counter list
-				visualize.setGreens(nums[i], i);
+				main.setGreens(nums[i], i);
 				arrayAccesses++;
 				delay(delay);
 			}
-			visualize.setGreens(0, 0);
+			main.setGreens(0, 0);
 			//now, we accumalate sum of our counter list
 			for (int i = 1; i < 10; i++) {
 				buckets[i] += buckets[i-1];
@@ -439,12 +439,12 @@ public class visualSort {
 				}
 				buckets[digit%10]--;
 				memoryNums[buckets[digit%10]] = nums[i];
-				visualize.updateIndex(memoryNums[buckets[digit%10]], buckets[digit%10]);
-				visualize.setReds(memoryNums[buckets[digit%10]], buckets[digit%10]);
+				main.updateIndex(memoryNums[buckets[digit%10]], buckets[digit%10]);
+				main.setReds(memoryNums[buckets[digit%10]], buckets[digit%10]);
 				arrayAccesses += 2;
 				delay(delay*2);
 			}
-			visualize.setReds(0, 0);
+			main.setReds(0, 0);
 			
 			//code go brr, more code but also more speed, however that works
 			digitFocus++;
@@ -461,11 +461,11 @@ public class visualSort {
 					digit /= 10;
 				}				
 				buckets[digit%10]++; //get the value of that digit we want to look at and at it to the counter list
-				visualize.setGreens(memoryNums[i], i);
+				main.setGreens(memoryNums[i], i);
 				arrayAccesses++;
 				delay(delay);
 			}
-			visualize.setGreens(0, 0);
+			main.setGreens(0, 0);
 			//now, we accumalate sum of our counter list
 			for (int i = 1; i < 10; i++) {
 				buckets[i] += buckets[i-1];
@@ -481,12 +481,12 @@ public class visualSort {
 				}
 				buckets[digit%10]--;
 				nums[buckets[digit%10]] = memoryNums[i];
-				visualize.updateIndex(nums[buckets[digit%10]], buckets[digit%10]);
-				visualize.setReds(nums[buckets[digit%10]], buckets[digit%10]);
+				main.updateIndex(nums[buckets[digit%10]], buckets[digit%10]);
+				main.setReds(nums[buckets[digit%10]], buckets[digit%10]);
 				arrayAccesses += 2;
 				delay(delay*2);
 			}
-			visualize.setReds(0, 0);
+			main.setReds(0, 0);
 		}
 		
 		return nums;
@@ -511,18 +511,19 @@ public class visualSort {
 	public static boolean check(int[] nums) {
 		for (int i = 0; i < nums.length-1; i++) {
 			if (nums[i] > nums[i+1]) {
-				visualize.setGreens(nums[i], i);
+				main.setGreens(nums[i], i);
 				return false;
 			}
 		}
-		visualize.setGreens(0, 0);
+		main.setGreens(0, 0);
 		return true;
 	}
 	
 	private static void delay(int msec) {
 		try {
 			Thread.sleep(msec);
-		} catch (InterruptedException e) {
+		}
+		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
